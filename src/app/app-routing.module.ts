597efaf8ad90422bc6b657f8c495be525d4ad506
Alaa Eddine Cherif <alaac617@gmail.com>
2023-01-17 14:44:24 +0100

    merge gone wrong

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
 import { UserLoginComponent } from './user-login/user-login.component';
 
 const routes: Routes = [
-  
-  {path : "register" , component : UserLoginComponent},
-  {path : "login" , component : SignUpComponent},
-  {path : "**" ,component : UserLoginComponent}
+  { path: 'register', component: UserLoginComponent },
+  { path: 'login', component: SignUpComponent },
+  { path: '**', component: HomeComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,12 @@ import { UserLoginComponent } from './user-login/user-login.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
 import { HttpClientModule } from '@angular/common/http';
 import { PostCardComponent } from './post-card/post-card.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { SearchFormComponent } from './search-form/search-form.component';
 
 @NgModule({
-  declarations: [AppComponent, UserLoginComponent, SignUpComponent, PostCardComponent],
+  declarations: [AppComponent, UserLoginComponent, SignUpComponent, PostCardComponent, NavbarComponent, HomeComponent, SearchFormComponent],
   imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
