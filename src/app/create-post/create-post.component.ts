636a7b64a44b37d375e97479b9be2dc9f2fecd83
Alaa Eddine Cherif <alaac617@gmail.com>
2023-01-17 21:16:00 +0100

    create-post

--- src/app/create-post/create-post.component.ts
+++ src/app/create-post/create-post.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-create-post',
   templateUrl: './create-post.component.html',
   styleUrls: ['./create-post.component.css'],
 })
-export class CreatePostComponent {}
+export class CreatePostComponent {
+  onsubmit(form: NgForm) {}
+}
